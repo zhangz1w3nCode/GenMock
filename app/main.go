340886f2b8d3c39aada13d091646e4c2061104cd
zhangz1w3nCode <403592973@qq.com>
2024-12-17 03:56:41 +0800

    feat:init.

--- app/main.go
+++ app/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"GenMock/app/server"
 	"net/http"
 )
 
 func main() {
 	// 注册 mock 接口
-	http.HandleFunc("/mock", server.MockHandler) // 使用 server.MockHandler
+	http.HandleFunc("/mock", MockHandler) // 使用 server.MockHandler
 
 	// 启动服务，监听 8080 端口
 	http.ListenAndServe(":8080", nil)
